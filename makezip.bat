e5e66db2a0b67b4f5c045e835cfb90548051edd8
Tigrou Ind <tigrou.ind@gmail.com>
2020-05-15 20:24:31 +0200

    Add zip batch file

---
+++ makezip.bat
@@ -0,0 +1,8 @@
+@echo off
+"%PROGRAMFILES%\7-Zip\7z" a -tzip "AITD-tools.zip" ^
+ ".\CacheViewer\CacheViewer\bin\Release\*.exe" ^
+ ".\LifeDISA\LifeDISA\bin\Release\*.exe" ^
+ ".\MemoryViewer\MemoryViewer\bin\Release\*.exe" ^
+ ".\MemoryViewer\MemoryViewer\bin\Release\*.dll" ^
+ ".\TrackDISA\TrackDISA\bin\Release\*.exe" ^
+ ".\VarsViewer\VarsViewer\bin\Release\*.exe" 
