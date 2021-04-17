6ccc14db3ccb8d4423b17b9d2674f96180cea63b
Tigrou Ind <tigrou.ind@gmail.com>
2021-04-17 20:19:19 +0200

    Display memory control blocks headers

--- MemoryViewer/MemoryViewer/Program.cs
+++ MemoryViewer/MemoryViewer/Program.cs
@@ -184,15 +184,20 @@ namespace MemoryViewer
 				bool inverse = true;
 				foreach (var block in DosBox.GetMCBs(pixelData))
 				{
-					int dest = block.Position - 16;
-					int length = Math.Min(block.Size + 16, pixels.Length - dest);
-	
 					uint color;					
 					if (block.Owner == 0) color = 0x90008000; //free
 					else if (block.Owner != psp) color = 0x90808000; 
 					else if (block.Position == psp) color = 0x90800000; //current executable
 					else color = inverse ? 0x900080F0 : 0x902000A0; //used					
+					
+					int dest = block.Position - 16;					
+					int length = Math.Min(16, pixels.Length - dest);					
+					for (int i = 0 ; i < length ; i++)
+					{
+						pixels[dest++] = 0x90FF00FF;
+					}
 	
+					length = Math.Min(block.Size, pixels.Length - dest);
 					for (int i = 0 ; i < length ; i++)
 					{
 						pixels[dest++] = color;
