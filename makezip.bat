235c801442101f65c6beb9fbac36338d60d67eb9
Tigrou Ind <tigrou.ind@gmail.com>
2021-10-08 23:59:29 +0200

    Update zip file

--- makezip.bat
+++ makezip.bat
@@ -4,6 +4,9 @@
  ".\LifeDISA\LifeDISA\bin\Release\*.exe" ^
  ".\LifeDISA\LifeDISA\bin\Release\UnPAK.dll" ^
  ".\MemoryViewer\MemoryViewer\bin\Release\*.exe" ^
- ".\MemoryViewer\MemoryViewer\bin\Release\*.dll" ^
+ ".\MemoryViewer\MemoryViewer\bin\Release\SDL.dll" ^
+ ".\MemoryViewer\MemoryViewer\bin\Release\SDL2.dll" ^
+ ".\MemoryViewer\MemoryViewer\bin\Release\SDL2_image.dll" ^
+ ".\MemoryViewer\MemoryViewer\bin\Release\Shared.dll" ^
  ".\TrackDISA\TrackDISA\bin\Release\*.exe" ^
  ".\VarsViewer\VarsViewer\bin\Release\*.exe" 
