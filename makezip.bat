5954ee27d0be48e8a8340b355cb862d62e60063d
Tigrou Ind <tigrou.ind@gmail.com>
2021-11-22 17:53:28 +0100

    Call msbuild from build script

--- makezip.bat
+++ makezip.bat
@@ -1,4 +1,16 @@
 @echo off
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\CacheViewer\CacheViewer.sln"
+if %ERRORLEVEL% NEQ 0 pause
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\LifeDISA\LifeDISA.sln"
+if %ERRORLEVEL% NEQ 0 pause
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\MemoryViewer\MemoryViewer.sln"
+if %ERRORLEVEL% NEQ 0 pause
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\TrackDISA\TrackDISA.sln"
+if %ERRORLEVEL% NEQ 0 pause
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\VarsViewer\VarsViewer.sln"
+if %ERRORLEVEL% NEQ 0 pause
+
 "%PROGRAMFILES%\7-Zip\7z" a -tzip "AITD-tools.zip" ^
  ".\CacheViewer\CacheViewer\bin\Release\*.exe" ^
  ".\LifeDISA\LifeDISA\bin\Release\*.exe" ^
@@ -9,4 +21,6 @@
  ".\MemoryViewer\MemoryViewer\bin\Release\SDL2_image.dll" ^
  ".\MemoryViewer\MemoryViewer\bin\Release\Shared.dll" ^
  ".\TrackDISA\TrackDISA\bin\Release\*.exe" ^
- ".\VarsViewer\VarsViewer\bin\Release\*.exe" 
+ ".\VarsViewer\VarsViewer\bin\Release\*.exe" ^
+ "-mx=9"
+if %ERRORLEVEL% NEQ 0 pause
