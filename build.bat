0ce9f08c15649ca6f2fef749e5f874a7e4b625a2
Tigrou Ind <tigrou.ind@gmail.com>
2022-04-01 11:29:07 +0200

    Get DLLs from appriopriate folders + force rebuild

--- build.bat
+++ build.bat
@@ -1,26 +1,28 @@
 @echo off
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\CacheViewer\CacheViewer.sln"
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\CacheViewer\CacheViewer.sln" /t:Rebuild
 if %ERRORLEVEL% NEQ 0 pause
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\LifeDISA\LifeDISA.sln"
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\LifeDISA\LifeDISA.sln" /t:Rebuild
 if %ERRORLEVEL% NEQ 0 pause
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\MemoryViewer\MemoryViewer.sln"
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\MemoryViewer\MemoryViewer.sln" /t:Rebuild
 if %ERRORLEVEL% NEQ 0 pause
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\TrackDISA\TrackDISA.sln"
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\TrackDISA\TrackDISA.sln" /t:Rebuild
 if %ERRORLEVEL% NEQ 0 pause
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\VarsViewer\VarsViewer.sln"
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\VarsViewer\VarsViewer.sln" /t:Rebuild
+if %ERRORLEVEL% NEQ 0 pause
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\Shared\Shared.csproj" /t:Rebuild
 if %ERRORLEVEL% NEQ 0 pause
 
 "%PROGRAMFILES%\7-Zip\7z" a -tzip "AITD-tools.zip" ^
  ".\CacheViewer\CacheViewer\bin\Release\*.exe" ^
  ".\LifeDISA\LifeDISA\bin\Release\*.exe" ^
- ".\LifeDISA\LifeDISA\bin\Release\UnPAK.dll" ^
  ".\MemoryViewer\MemoryViewer\bin\Release\*.exe" ^
  ".\MemoryViewer\MemoryViewer\bin\Release\SDL.dll" ^
  ".\MemoryViewer\MemoryViewer\bin\Release\SDL2.dll" ^
  ".\MemoryViewer\MemoryViewer\bin\Release\SDL2_image.dll" ^
- ".\MemoryViewer\MemoryViewer\bin\Release\Shared.dll" ^
  ".\TrackDISA\TrackDISA\bin\Release\*.exe" ^
  ".\VarsViewer\VarsViewer\bin\Release\*.exe" ^
+ ".\Shared\bin\Release\Shared.dll" ^
+ ".\UnPAK\bin\Release\Unpak.dll" ^
  "-mx=9"
 if %ERRORLEVEL% NEQ 0 pause
