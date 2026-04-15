116aea71f7ca4fe4425c5fb415d7f0516768ac51
Tigrou Ind <tigrou.ind@gmail.com>
2026-04-15 13:35:42 +0200

    Refactor

--- PAKExtract/PAKExtract/Archive.cs
+++ PAKExtract/PAKExtract/Archive.cs
@@ -1,7 +1,6 @@
 using Shared;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -13,23 +12,7 @@ namespace PAKExtract
 	{
 		public static void CreateArchive(string[] args, bool timegate)
 		{
-			string dosBoxPath = null;
-
-			if (File.Exists("pkzip.exe")) //cannot use DOSBox compression without PKZIP.exe
-			{
-				var searchDirectories = new string[]
-				{
-					".",
-					Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
-					Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86)
-				};
-
-				dosBoxPath = searchDirectories
-				.SelectMany(x => Directory.GetDirectories(x))
-					.Where(x => x.Contains("dosbox", StringComparison.InvariantCultureIgnoreCase))
-					.Select(x => Path.Combine(x, "dosbox.exe"))
-					.FirstOrDefault(x => File.Exists(x));
-			}
+			string dosBoxPath = DosBoxZip.GetDosBoxPath();
 
 			foreach (var folder in args)
 			{
@@ -49,161 +32,73 @@ namespace PAKExtract
 				var entries = new List<PakArchiveEntry>();
 
 				PakArchiveEntry[] existingEntries = [];
-				if (File.Exists(pakFile)) //try to see if file has been changed, if not use file from already existing PAK archive
+				if (File.Exists(pakFile)) //load all entries to compare them with files to be compressed and see if it has changed
 				{
 					existingEntries = PakArchive.Load(pakFile);
 				}
 
+				//try to see if file has been changed, if not use file from already existing PAK archive
 				var filesToCompressWithDOSBox = new List<(string FilePath, PakArchiveEntry Entry)>();
 				foreach (var filePath in Directory.EnumerateFiles(folder, "*.*"))
 				{
-					var data = File.ReadAllBytes(filePath);
-					var extraPath = Path.Combine(Path.GetDirectoryName(filePath)!, "EXTRA", Path.GetFileName(filePath));
-					var extra = File.Exists(extraPath) ? File.ReadAllBytes(extraPath) : [];
-
-					byte[] compressedData = data;
-					byte compressionType = 0; //uncompressed
-					if (timegate)
+					entries.Add(GetEntry());
+					
+					PakArchiveEntry GetEntry()
 					{
-						using var memoryStream = new MemoryStream();
-						using (var deflateStream = new DeflateStream(memoryStream, CompressionMode.Compress))
-						{
-							deflateStream.Write(data, 0, data.Length);
-						}
+						var data = File.ReadAllBytes(filePath);
+						var extraPath = Path.Combine(Path.GetDirectoryName(filePath)!, "EXTRA", Path.GetFileName(filePath));
+						var extra = File.Exists(extraPath) ? File.ReadAllBytes(extraPath) : [];
 
-						compressedData = memoryStream.ToArray();
-						compressionType = 4; //gzip
-					}
-					else
-					{
-						var filename = Path.GetFileNameWithoutExtension(filePath);
-						if (Regex.IsMatch(filename, "[0-9]+"))
+						byte[] compressedData = data;
+						byte compressionType = 0; //uncompressed
+						if (timegate)
 						{
-							int entryIndex = int.Parse(filename);
-							if (entryIndex >= 0 && entryIndex < existingEntries.Length)
+							using var memoryStream = new MemoryStream();
+							using (var deflateStream = new DeflateStream(memoryStream, CompressionMode.Compress))
 							{
-								var entry = existingEntries[entryIndex];
-								if (Enumerable.SequenceEqual(entry.Read(), data)) //file unmodified
+								deflateStream.Write(data, 0, data.Length);
+							}
+
+							compressedData = memoryStream.ToArray();
+							compressionType = 4; //gzip
+						}
+						else
+						{
+							var filename = Path.GetFileNameWithoutExtension(filePath);
+							if (Regex.IsMatch(filename, "[0-9]+"))
+							{
+								int entryIndex = int.Parse(filename);
+								if (entryIndex >= 0 && entryIndex < existingEntries.Length)
 								{
-									entries.Add(entry); //leave untouched
-									continue;
+									var entry = existingEntries[entryIndex];
+									if (Enumerable.SequenceEqual(entry.Read(), data)) //file unmodified
+									{
+										return entry; //untouched
+									}
 								}
+							}
+
+							if (!string.IsNullOrEmpty(dosBoxPath))
+							{
+								var entry = new PakArchiveEntry(data.Length, extra, compressedData, compressionType);
+								filesToCompressWithDOSBox.Add((filePath, entry)); //for later on
+								return entry; //untouched
 							}
 						}
 
-						if (!string.IsNullOrEmpty(dosBoxPath))
-						{
-							var entry = new PakArchiveEntry(data.Length, extra, compressedData, compressionType);
-							filesToCompressWithDOSBox.Add((filePath, entry)); //for later on
-							entries.Add(entry); //might be modified later, after DOSBOX compression
-							continue;
-						}
+						return new PakArchiveEntry(data.Length, extra, compressedData, compressionType);
 					}
-
-					entries.Add(new PakArchiveEntry(data.Length, extra, compressedData, compressionType));
 				}
 
 				if (filesToCompressWithDOSBox.Any())
 				{
-					var tempDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-					Directory.CreateDirectory(tempDirectory);
-					Directory.CreateDirectory(Path.Combine(tempDirectory, "FILES"));
-
-					try
-					{
-						//copy pkzip and all files to be compressed to temporary folder
-						File.Copy("pkzip.exe", Path.Combine(tempDirectory, "pkzip.exe"));
-						foreach (var (filePath, _) in filesToCompressWithDOSBox)
-						{
-							File.Copy(filePath, Path.Combine(tempDirectory, "FILES", Path.GetFileName(filePath)));
-						}
-
-						var destFile = Path.Combine(tempDirectory, "temp.zip");
-						RunDOSBox(dosBoxPath, destFile);
-						if (File.Exists(destFile))
-						{
-							var data = File.ReadAllBytes(destFile);
-							var result = GetCompressedDataFromZip(data);
-
-							foreach (var (filePath, entry) in filesToCompressWithDOSBox)
-							{
-								//some entries might be missing (eg: file wasn't compressed because it make no sense (eg: stored compression type)
-								if (result.TryGetValue(Path.GetFileName(filePath), out var compressedInfo))
-								{
-									var (compressedData, uncompressedSize) = compressedInfo;
-									if (uncompressedSize == entry.UncompressedSize)
-									{
-										entry.Write(compressedData);
-										entry.CompressionType = 1;
-									}
-								}
-							}
-						}
-					}
-					finally
-					{
-						Directory.Delete(tempDirectory, true);
-					}
+					DosBoxZip.CompressWithDosBox(dosBoxPath, filesToCompressWithDOSBox); //will modify "entries" indirectly
 				}
 
 				Program.WriteFile(pakFile, PakArchive.Save(entries));
 			}
-		}
 
-		static Dictionary<string, (byte[] ComparessedData, int UncompressedSize)> GetCompressedDataFromZip(byte[] data)
-		{
-			int offset = 0;
-			var result = new Dictionary<string, (byte[] ComparessedData, int UncompressedSize)>(StringComparer.InvariantCultureIgnoreCase);
-			while ((offset + 0x20) <= data.Length &&
-					Tools.ReadUnsignedInt(data, offset + 0) == 0x04034b50) //PKZIP header (v1.1 should be used)
-			{
-				var compressionType = data.ReadUnsignedShort(offset + 0x08);
-				var compressedSize = data.ReadUnsignedShort(offset + 0x12);
-				var uncompressedSize = data.ReadUnsignedShort(offset + 0x16);
-				var fileNameLen = data.ReadUnsignedShort(offset + 0x1a);
-				var extraLen = data.ReadUnsignedShort(offset + 0x1c);
-				var fileName = data.ReadString(offset + 0x1e, fileNameLen);
-
-				offset += 0x1e + fileNameLen + extraLen;
-				if ((offset + compressedSize) <= data.Length
-					&& compressionType == 6  //implode
-					&& Tools.ReadUnsignedInt(data, offset) == 0x0312000F) //implode signature
-				{
-					result[fileName] = ([.. data.AsSpan(offset, compressedSize)], uncompressedSize);
-				}
-
-				offset += compressedSize;
-			}
-
-			return result;
-		}
-
-		static void RunDOSBox(string dosboxExePath, string directory)
-		{
-			var psi = new ProcessStartInfo
-			{
-				FileName = dosboxExePath,
-				UseShellExecute = false,
-				CreateNoWindow = false,
-			};
-
-			psi.ArgumentList.Add("-c");
-			psi.ArgumentList.Add("cycles max");
-
-			psi.ArgumentList.Add("-c");
-			psi.ArgumentList.Add($"mount c \"{Path.GetDirectoryName(directory)}\"");
-
-			psi.ArgumentList.Add("-c");
-			psi.ArgumentList.Add("c:");
-
-			psi.ArgumentList.Add("-c");
-			psi.ArgumentList.Add($"pkzip -ei C:\\{Path.GetFileName(directory)} C:\\FILES\\*.*");
-
-			psi.ArgumentList.Add("-c");
-			psi.ArgumentList.Add("exit");
-
-			using var p = Process.Start(psi);
-			p.WaitForExit();
+			
 		}
 	}
 }
