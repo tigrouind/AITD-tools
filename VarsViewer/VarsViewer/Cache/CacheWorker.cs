152975e4445c4129babf35de4e7384bbb6a45ba6
Tigrou Ind <tigrou.ind@gmail.com>
2026-09-25 14:30:01 +0200

    Fix crash when JITD (not supported) is running

--- VarsViewer/VarsViewer/Cache/CacheWorker.cs
+++ VarsViewer/VarsViewer/Cache/CacheWorker.cs
@@ -38,7 +38,7 @@ namespace VarsViewer
 			foreach (var ch in Cache)
 			{
 				int cachePointer = Program.Memory.ReadFarPointer(Program.EntryPoint + GameConfig[ch.Index]);
-				if (cachePointer != 0 && cachePointer < Program.Memory.Length) //should never happen, unless game crashes
+				if (cachePointer != 0 && cachePointer >= 16 && cachePointer < Program.Memory.Length) //should never happen, unless game crashes
 				{
 					DosMCB block = DosMCB.ReadMCB(Program.Memory, cachePointer - 16);
 					if ((block.Tag == 0x4D || block.Tag == 0x5A) && block.Owner != 0 && block.Size < 4096) //block is still allocated
